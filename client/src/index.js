3cc2d304b666739384de35ebb43839c6e524ac2c
Regina Agunod <53545239+ReginaAgunod@users.noreply.github.com>
2020-09-02 17:18:43 -0700

    Added import for ReactDOM to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'; 
+// Added ReactDOM import
+import { BrowserRouter as Router } from "react-router-dom";
 
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+rootElement);
