e546cd9bb557ffb737a0808af1972abf24c1e0a7
Regina Agunod <53545239+ReginaAgunod@users.noreply.github.com>
2020-09-06 14:25:52 -0700

    Added functionality and finished up code to meet MVP

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import MovieCard from "./MovieCard";
 
 const MovieList = props => {
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <div key = {movie.id}>
+        <Link to={`/movies/${movie.id}`}>
+          <MovieDetails key={movie.id} movie={movie} />
+        </Link>
+        </div>
       ))}
     </div>
   );
 }
 
-function MovieDetails({ movie }) {
+function MovieDetails(props) {
+  const movie = props.movie;
   const { title, director, metascore } = movie;
   return (
     <div className="movie-card">
+      <Link to={`/movie/${movie.id}`}>
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -21,6 +29,7 @@ function MovieDetails({ movie }) {
       <div className="movie-metascore">
         Metascore: <strong>{metascore}</strong>
       </div>
+      </Link>
     </div>
   );
 }
